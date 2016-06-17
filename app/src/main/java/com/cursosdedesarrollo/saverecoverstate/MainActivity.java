97d8320a17ad90648d34dd2ca6c0ac2692f8b138
pepesan <pepesan@gmail.com>
2016-06-17 13:49:38 +0200

    Initial commit

---
+++ app/src/main/java/com/cursosdedesarrollo/saverecoverstate/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cursosdedesarrollo.saverecoverstate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private Calendar startTime = Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void salta(View v){
+        Intent i=new Intent(getApplicationContext(), Main2Activity.class);
+        startActivity(i);
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putSerializable("starttime", startTime);
+        Log.d("app",""+startTime);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        startTime = (Calendar) savedInstanceState.getSerializable("starttime");
+        Log.d("app",""+startTime);
+    }
+}
